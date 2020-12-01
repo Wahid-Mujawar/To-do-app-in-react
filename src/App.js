8af3d053016e61279f2e609d9c93249c9f6d1e64
Wahid-Mujawar <abdulwahid0218@gmail.com>
2020-12-01 13:51:19 -0800

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ListItems from './ListItems';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {faTrash} from '@fortawesome/free-solid-svg-icons';
 
+library.add(faTrash);
 
 class App extends React.Component {
   constructor(props){
@@ -16,6 +19,7 @@ class App extends React.Component {
     }
     this.handleInput = this.handleInput.bind(this);
     this.addItem = this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
   }
   handleInput(e){
     this.setState({
@@ -40,6 +44,14 @@ class App extends React.Component {
       })
     }
   }
+deleteItem(key){
+  const filteredItems = this.state.items.filter(item => 
+    item.key!==key);
+    this.setState({
+      items:filteredItems
+    })
+}
+
   render(){
    return (
     <div className="App">
@@ -49,7 +61,9 @@ class App extends React.Component {
         <button type="submit">Add</button>
       </form>
     </header>
-    <ListItems items = {this.state.items}></ListItems>
+    <ListItems items = {this.state.items} 
+    deleteItem={this.deleteItem}>
+    </ListItems>
 
     </div>
     );
