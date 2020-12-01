a47b590729ed13bd340bdc9920d98429483f9e97
Wahid-Mujawar <abdulwahid0218@gmail.com>
2020-11-30 20:06:33 -0800

    test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListItems from './ListItems';
+
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:'',
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem = this.addItem.bind(this);
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text:e.target.value,
+        key:Date.now()
+      }
+    })
+  }
+    addItem(e){
+      e.preventDefault();
+      const newItem = this.state.currentItem;
+      console.log(newItem);
+      if(newItem.text!==""){
+        const newItems =[...this.state.items, newItem]; 
+        this.setState({
+          items:newItems,
+          currentItem:{
+          text:'',
+          key:'',
+        }
+      })
+    }
+  }
   render(){
    return (
+    <div className="App">
     <header>
-      <div className="App">
-      <form id="to-do-form">
-        <input type="text" placeholder="Enter Text"></input>
+      <form id="to-do-form" onSubmit={this.addItem}>
+        <input type="text" placeholder="Enter Text" value={this.state.currentItem.text} onChange={this.handleInput}></input>
         <button type="submit">Add</button>
       </form>
-      </div>
     </header>
+    <ListItems items = {this.state.items}></ListItems>
+
+    </div>
     );
   }
 }
